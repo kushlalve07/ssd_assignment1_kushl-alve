768dd4e825fc0158acdc1350d0cc18d7365623af
Kushl Alve <alvekushl@gmail.com>
2026-08-16 12:30:46 +0530

    Add StudentProfile struct and printProfile function

---
+++ profile.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+struct StudentProfile {
+    std::string name;
+    std::string rollNumber;
+    std::string email;
+    std::string major;
+    int yearOfStudy;
+};
+
+void printProfile(const StudentProfile& profile) {
+    std::cout << "----- Student Profile -----" << std::endl;
+    std::cout << "Name        : " << profile.name << std::endl;
+    std::cout << "Roll Number : " << profile.rollNumber << std::endl;
+    std::cout << "Email       : " << profile.email << std::endl;
+    std::cout << "Major       : " << profile.major << std::endl;
+    std::cout << "Year        : " << profile.yearOfStudy << std::endl;
+    std::cout << "----------------------------" << std::endl;
+}
